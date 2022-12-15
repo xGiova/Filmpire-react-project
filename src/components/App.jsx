d800aed415b6c71b926c5207dead9473a3025023
ADS Musciagli Giovanni <adsmusciag@alpitourworld.it>
2022-12-15 12:48:29 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+import { Actors, MovieInformation, Profile, Movies, NavBar } from './index';
+import useStyles from './styles';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Movies = () => {
+  console.log('Movies');
+  return (
+    <div>
+      <h1>Movies</h1>
+    </div>
+  );
+};
+
+export default Movies;
